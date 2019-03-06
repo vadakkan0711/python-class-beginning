c0b84588ab81cc6bee47117a514d70d5657cb197
vadakkan0711 <immanuelvadakedam@gmail.com>
2019-03-06 13:38:48 +0530

    app

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask,render_template
+app=Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+@app.route("/home")
+def home():
+    return "welcome to OOTY,NICE TO MEET U"
+  
+if(__name__=="__main__"):
+    app.run(debug=True)
+
